0bc25a888b08ca2ffc7a372e6fdd67612fc41a88
Poresh181 <poresh450@gmail.com>
2025-11-10 20:07:54 +0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas
 import numpy
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -10,3 +11,13 @@ st.subheader("Data Science With Poresh")
 
 #sidebar
 st.sidebar.header("Upload CSV Data Or Use Sample")
+use_example = st.sidebar.checkbox("Use Example Dataset")
+
+#Load Data
+if use_example:
+  df = sns.load_dataset('tips')
+  df = df.dropna()
+  st.success("Loaded sample dataset:'tips'")
+else:
+  st.sidebar.file_uploader("Upload your CSV file")
+  
